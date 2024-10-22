c130ab2d44cfb543569df344677925a50285aad6
Thomas Reese <thomas18reese@gmail.com>
2024-10-22 02:19:27 -0500

    updating resume link

--- thomas-portfolio-project/src/components/Navbar/Navbar.tsx
+++ thomas-portfolio-project/src/components/Navbar/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
 					))}
 				</ul>
 				<a
-					href='https://drive.google.com/file/d/1Xxme-Q0HY299yebjTTyeKw7af02v-jCT/view?usp=drive_link'
+					href='https://drive.google.com/file/d/1Xxme-Q0HY299yebjTTyeKw7af02v-jCT/view?usp=sharing'
 					target='_blank'
 					rel='noreferrer'
 					className='navbar__button'
